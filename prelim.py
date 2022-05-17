834ef9a7c1392fa92e2c48f2114ec4ee6bed6c70
Koh <xxx>
2022-05-17 16:28:05 -0700

    Cleaned up implementation for inserting a record

--- prelim.py
+++ prelim.py
@@ -2,58 +2,38 @@ from numpy import record
 from ndn.app import NDNApp
 from ndn.encoding.name import Name
 
+# TODO: Implement the hash function
+def hash(self, input):
+    return input
+
 # basic building block code for the resource records
 class Logger(NDNApp):
     def __init__(self, logger_ID):
         NDNApp.__init__(face=None, keychain=None)
-        self.logger_ID = logger_ID # TODO
+        self.logger_ID = logger_ID # TODO: Assign the logger ID properly
         self.record_list = []
         self.tails_list = []
         return
-    
-    # TODO
-    def hash(self, input):
-        return input
 
-    def receive_event(self, log_event):
-        # self.tails_list = svs.receive(tails_list)
-        prior_node_1 = self.tails_list.pop()
-        prior_node_2 = self.tails_list.pop()
-        new_record = self.create_record(log_event, prior_node_1, prior_node_2)
+    # Given a log event, create and return an NDN record packet.
+    def create_record(self, log_event, prior_node_1, prior_node_2):
+        record_name = Name.from_str(self.logger_ID + "/" + "RECORD" + "/" + log_event) # TODO: Naming packet using correct convention
+        payload = { "log_event": log_event, 
+                    "prior_digest_1": hash(prior_node_1),
+                    "prior_digest_2": hash(prior_node_2)
+                }
+        return self.prepare_data(record_name, content=payload, signer=None) # TODO: Signing packet
+
+    # Given a log event, create an NDN record packet
+    # Push the record to the DAG
+    # Send the new information about the DAG out on SVS
+    def insert_record(self, log_event):
+        # tails_list = svs.update(tails_list)
+        # record_list = svs.update(record_list)
+        prior_node_1 = self.tails_list.pop() if (len(self.tails_list) > 0) else None
+        prior_node_2 = self.tails_list.pop() if (len(self.tails_list) > 0) else None
+        new_record = self.create_record(log_event,prior_node_1, prior_node_2)
         self.record_list.append(new_record)
         self.tails_list.append(new_record)
         # svs.publish(tails_list)
-
-    def create_record(self, log_event, prior_node_1, prior_node_2):
-        record_name = Name.from_str(self.logger_ID + "/" + "RECORD" + "/" + log_event) # TODO
-        prior_digest_1 = hash(prior_node_1)
-        prior_digest_2 = hash(prior_node_2)
-        payload = { "log_event": log_event, 
-                    "prior_digest_1": prior_digest_1, 
-                    "prior_digest_2": prior_digest_2
-                }
-        self.prepare_data(record_name, content=payload, signer=None) #TODO: signing
-        return {"rec_name": record_name, "rec_payload": payload}
-    def create_record(self, log_event):
-        # SVS event
-        record_name = Name.from_str(self.logger_ID + "/" + "RECORD" + log_event) # TODO: replace log_event.id with an actual identifier from the log event received
-        # TODO: assign prior record digests based on the tails + turn them into digests
-        # TODO: conditional logic depending on whether two tails are even available
-        payload = {"log_event": log_event, "prior_digest_1": None, "prior_digest_2": None}
-        # TODO: have the logger sign the record properly
-        self.prepare_data(record_name, content=payload, signer=None)
-
-
-    def insert_record(self, record_event):
-        if len(self.record_list) == 0:
-            record_event["payload"]["prior_digest_1"] = record_event
-            record_event["payload"]["prior_digest_2"] = record_event
-        elif len(self.record_list) == 1:
-            record_event["payload"]["prior_digest_1"] = self.record_list[-1]
-            record_event["payload"]["prior_digest_2"] = self.record_list[-1]
-        else:
-            record_event["payload"]["prior_digest_1"] = self.record_list[-1]
-            record_event["payload"]["prior_digest_2"] = self.record_list[-2]
-        self.record_list.insert(len(self.record_list), record_event)
-
-
+        # svs.publish(record_list)
