582dd995b615abe7198a1668adb0449f7399c3c0
Dianna C <emkoh@protonmail.com>
2022-05-21 11:27:10 -0700

    Added code for record svs group

--- app.py
+++ app.py
@@ -14,56 +14,51 @@ app = NDNApp()
 
 def parse_cmd_args() -> dict:
     # Command Line Parser
-    parser = ArgumentParser(add_help=False,description="Logger node receiving log events and putting them into a DAG as records.")
+    parser = ArgumentParser(add_help=False,description="An SVS Integer Counting Node capable of syncing with others.")
     requiredArgs = parser.add_argument_group("required arguments")
     optionalArgs = parser.add_argument_group("optional arguments")
     informationArgs = parser.add_argument_group("information arguments")
     # Adding all Command Line Arguments
     requiredArgs.add_argument("-n", "--nodename",action="store",dest="node_name",required=True,help="id of this node in svs")
+    optionalArgs.add_argument("-gp","--groupprefix",action="store",dest="group_prefix",required=False,help="overrides config | routable group prefix to listen from")
+    optionalArgs.add_argument("-i","--interval",action="store",dest="interval",type=int,default=5,required=False,help="interval at which data is published")
     optionalArgs.add_argument("-v","--verbose",action="store_true",dest="verbose",default=False,required=False,help="when set, svsync info is displayed as well")
     informationArgs.add_argument("-h","--help",action="help",default=SUPPRESS,help="show this help message and exit")
     # Getting all Arguments
     argvars = parser.parse_args()
     args = {}
+    args["group_prefix"] = argvars.group_prefix if argvars.group_prefix is not None else "/svs"
     args["node_id"] = argvars.node_name
     args["verbose"] = argvars.verbose
+    args["interval"] = argvars.interval
     return args
 
 class Program:
     def __init__(self, args:dict) -> None:
         self.args = args
-        self.group_prefix = "/svs/log_events"
-        self.records_group_prefix = "/svs/records"
-        self.svs:SVSync = SVSync(app, Name.from_str(self.group_prefix), Name.from_str(self.args["node_id"]), self.missing_callback)
-        self.svs_records = SVSync(app, Name.from_str(self.records_group_prefix), Name.from_str(self.args["node_id"]), self.records_missing_callback)
-        self.records_list = []
-        print(f'SVS Consumer started! | group prefix: {self.group_prefix}, records group prefix: {self.records_group_prefix},  node id: {self.args["node_id"]} |')
-    
+        self.svs:SVSync = SVSync(app, Name.from_str(self.args["group_prefix"]), Name.from_str(self.args["node_id"]), self.missing_callback)
+        print(f'SVS count client started | {self.args["group_prefix"]} - {self.args["node_id"]} |')
     async def run(self) -> None:
-        return
-    
+        num:int = 0
+        while 1:
+            num = num+1
+            try:
+                print("YOU: "+str(num))
+                self.svs.publishData(str(num).encode())
+            except KeyboardInterrupt:
+                sys.exit()
+            await aio.sleep(self.args["interval"])
     def missing_callback(self, missing_list:List[MissingData]) -> None:
         aio.ensure_future(self.on_missing_data(missing_list))
-    
     async def on_missing_data(self, missing_list:List[MissingData]) -> None:
         for i in missing_list:
             while i.lowSeqno <= i.highSeqno:
                 content_str:Optional[bytes] = await self.svs.fetchData(Name.from_str(i.nid), i.lowSeqno, 2)
                 if content_str:
-                    self.records_list.append(content_str.decode())
-                    print(self.records_list)
-                i.lowSeqno = i.lowSeqno + 1
-    
-    def records_missing_callback(self, missing_list:List[MissingData]) -> None:
-        aio.ensure_future(self.records_on_missing_data(missing_list))
-    
-    async def records_on_missing_data(self, missing_list:List[MissingData]) -> None:
-        for i in missing_list:
-            while i.lowSeqno <= i.highSeqno:
-                content_str:Optional[bytes] = await self.svs.fetchData(Name.from_str(i.nid), i.lowSeqno, 2)
-                if content_str:
-                    self.records_list.append(content_str.decode())
-                    print(self.records_list)
+                    output_str:str = i.nid + ": " + content_str.decode()
+                    sys.stdout.write("\033[K")
+                    sys.stdout.flush()
+                    print(output_str)
                 i.lowSeqno = i.lowSeqno + 1
 
 async def start(args:dict) -> None:
@@ -73,6 +68,7 @@ async def start(args:dict) -> None:
 def main() -> int:
     args = parse_cmd_args()
     args["node_id"] = Name.to_str(Name.from_str(args["node_id"]))
+    args["group_prefix"] = Name.to_str(Name.from_str(args["group_prefix"]))
 
     SVSyncLogger.config(True if args["verbose"] else False, None, logging.INFO)
 
