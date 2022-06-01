63de8ff025c820240dd58d123c6c7ad9c9470d9d
root <emkoh@protonmail.com>
2022-06-01 20:58:37 +0000

    Split record storage, added ndn interest and service

--- consumer.py
+++ consumer.py
@@ -48,14 +48,16 @@ class RecordStorage:
         
     def get_record(self, record_name):
         if record_name in self.records_list:
-            return {record_name: self.records_list[record_name]}
+            return self.records_list[record_name]
         return None
+
+record_storage = RecordStorage()
 
 class Consumer:
     def __init__(self, args:dict) -> None:
         self.args = args
-        self.record_storage = RecordStorage()
         self.record_changes = []
+        self.interest_queue = []
         # log_events group related (communication between producer and loggers)
         self.log_events_group_prefix = "/svs/mnemosyne/log_events"
         self.svs_log_events:SVSync = SVSync(app, Name.from_str(self.log_events_group_prefix), Name.from_str(self.args["node_id"]), self.log_events_missing_callback)
@@ -63,14 +65,40 @@ class Consumer:
         self.records_group_prefix = "/svs/mnemosyne/records"
         self.svs_records:SVSync = SVSync(app, Name.from_str(self.records_group_prefix), Name.from_str(self.args["node_id"]), self.records_missing_callback)
         print(f'CONSUMER STARTED! | LOG GROUP PREFIX: {self.log_events_group_prefix} | RECORDS GROUP PREFIX {self.records_group_prefix} | NODE ID: {self.args["node_id"]} |')
-    
+
     async def run(self) -> None:
-        pass
+        if (len(self.interest_queue) > 0):
+            try:
+                data_name, meta_info, content = await app.express_interest(
+                    # Interest Name
+                    '/mnemosyne/record_interest' + self.interest_queue.pop(),
+                    must_be_fresh=True,
+                    can_be_prefix=False,
+                    lifetime=6000)
+                record_storage.store_record({Name.to_str(data_name):bytes(content)})
+            except InterestNack as e:
+                # A NACK is received
+                print(f'Nacked with reason={e.reason}')
+            except InterestTimeout:
+                # Interest times out
+                print(f'Timeout')
+            except InterestCanceled:
+                # Connection to NFD is broken
+                print(f'Canceled')
+            except ValidationFailure:
+                # Validation failure
+                print(f'Data failed to validate')
+
+    @app.route('/mnemosyne/record_interest')
+    def on_interest(name, interest_param, application_param):
+        retrieved_record = record_storage.get_record(name)
+        if retrieved_record:
+            app.put_data(name, content=retrieved_record, freshness_period=10000)
 
     # Given a log event, create and return an NDN record packet.
     # TODO: This is a temporary implementation. Should actually convert the stuff to packets.
     def create_record(self, log_event):
-        prior_records = [tail for tail in [self.record_storage.pop_tail_record(), self.record_storage.pop_tail_record()] if tail]
+        prior_records = [tail for tail in [record_storage.pop_tail_record(), record_storage.pop_tail_record()] if tail]
         # Log the tail records removed
         for prior_record in prior_records:
             self.record_changes.append("DEL-TAIL " + prior_record)
@@ -79,8 +107,7 @@ class Consumer:
     # Creates and stores a record. Notes down the record changes to be published by SVS.
     def receive_log_event(self, content_str):
         new_record = self.create_record(content_str.decode())
-        self.record_storage.store_record(new_record)
-        # TODO: Note and return actions taken, publish record changes.
+        record_storage.store_record(new_record)
         self.record_changes.append("ADD-REC " + content_str.decode())
         self.publish_record_changes()
 
@@ -94,10 +121,9 @@ class Consumer:
         for record_change in record_change_list:
             split_change = record_change.split(" ")
             if split_change[0] == "ADD-REC":
-                new_record = {split_change[1]:["ah","ah"]}# TODO: express NDN interest and store it in the record storage when it is answered
-                self.record_storage.store_record(new_record)
+                new_record = self.interest_queue.append(split_change[1])
             elif split_change[0] == "DEL-TAIL":
-                self.record_storage.pop_tail_record(split_change[1])
+                record_storage.pop_tail_record(split_change[1])
         return
 
     def log_events_missing_callback(self, missing_list:List[MissingData]) -> None:
